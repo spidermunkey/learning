15ab9fa713da61aa2e01f1e45f9927b8ce818fe6
spidermunkey <burkstremane@gmail.com>
2024-01-26 11:53:51 -0700

    config course deps

---
+++ adopt-me/src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { createRoot } from "react-dom";
+
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  // component must always return markup
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Luna",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      animal: "Bird",
+      name: "Pepper",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Doink",
+      breed: "Mixed",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
